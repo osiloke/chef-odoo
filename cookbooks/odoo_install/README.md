f55581baa1fb811aeb474b65e7ca2c1d05102d6d
mdoederlein <michael.doederlein@cloud-logic.net>
2016-03-30 17:56:30 +0200

    new cookbook - odoo_install

---
+++ cookbooks/odoo_install/README.md
@@ -0,0 +1,4 @@
+# odoo_install
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/odoo_install/metadata.rb
@@ -0,0 +1,7 @@
+name 'odoo_install'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures odoo_install'
+long_description 'Installs/Configures odoo_install'
+version '0.1.0'
